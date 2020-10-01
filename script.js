7bf0f2cf3ad41d7ff46190e50e0cccbdbf5c8d91
bekzod <you@example.com>
2020-10-01 20:16:56 +0500

    js question

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var name = prompt('type your name');
+console.log('Welcome ' + name );
+
+var year = prompt('your age');
+console.log('i am ' + year + ' years old');
+
+var q1 = prompt('answer the question 12 + 3');
+console.log(12 + 3 +' your answer-' + q1) ;
+
+var q2 = prompt('answer the question 5*250');
+console.log(5*250 +' your answer-' + q2);
+
+var q3 = prompt('answer the question 120/2');
+console.log(120/2 + 'your answer '+q3);
+
+var q4 = prompt('answ   er the last question 111-111')
+console.log( 111-111 +' your answer '+ q4);
